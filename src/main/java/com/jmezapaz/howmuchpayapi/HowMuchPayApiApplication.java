a5921d420b416309457c5b42186fc41f09bd4afc
Jorge Meza <jorge.meza.paz@gmail.com>
2024-02-13 14:04:47 -0300

    Revert "reset"
    This reverts commit 5092fa97f0cd1d02f2f7b057ebe733f9f0d8c894.

---
+++ src/main/java/com/jmezapaz/howmuchpayapi/HowMuchPayApiApplication.java
@@ -0,0 +1,13 @@
+package com.jmezapaz.howmuchpayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowMuchPayApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HowMuchPayApiApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/jmezapaz/howmuchpayapi/controller/ServiceApi.java
@@ -0,0 +1,35 @@
+package com.jmezapaz.howmuchpayapi.controller;
+
+import com.jmezapaz.howmuchpayapi.dto.TestDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/services")
+public class ServiceApi {
+
+  private static Logger LOGGER = LoggerFactory.getLogger(ServiceApi.class);
+
+  @GetMapping
+  public ResponseEntity<List<String>> services(){
+    List<String> list = new ArrayList<>();
+    list.add("Netflix");
+    list.add("Youtube");
+    list.add("Spotify");
+    list.add("Amazon Prime");
+    LOGGER.info("--ServiceApi::services services:{}", list);
+    return new ResponseEntity<>(list, HttpStatus.OK);
+  }
+
+  @PostMapping
+  public ResponseEntity<TestDto> save(@RequestBody TestDto request){
+    LOGGER.info("--ServiceApi::save request:{}", request);
+    return new ResponseEntity<>(request, HttpStatus.OK);
+  }
+}
