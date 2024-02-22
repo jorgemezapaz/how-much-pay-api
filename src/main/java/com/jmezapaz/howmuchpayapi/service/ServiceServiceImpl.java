d95d98ea7700ae6ca69e3d7c276a465eb142cb30
Jorge Meza <jorge.meza.paz@gmail.com>
2024-02-22 14:27:40 -0300

    error get json data exception

--- src/main/java/com/jmezapaz/howmuchpayapi/service/ServiceServiceImpl.java
+++ src/main/java/com/jmezapaz/howmuchpayapi/service/ServiceServiceImpl.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -20,11 +19,11 @@ public class ServiceServiceImpl implements ServiceService{
     try{
       String s = new String(Files.readAllBytes(Paths.get("src/main/resources/data/data.json")));
       ObjectMapper mapper = new ObjectMapper();
-      List<ServiceDTO> services = mapper.readValue(s, new TypeReference<List<ServiceDTO>>() {});
+      List<ServiceDTO> services = mapper.readValue(s, new TypeReference<>() {});
       LOGGER.info("--ServiceServiceImpl::getAllService services: {}", services);
       return services;
-    } catch (IOException e){
-      LOGGER.error("--ServiceServiceImpl::getAllService services: {} {}", e.getMessage(), e.getCause());
+    } catch (Exception e){
+      LOGGER.error("--ServiceServiceImpl::getAllService services: {} - {}", e.getMessage(), e.getCause());
     }
     return null;
   }
